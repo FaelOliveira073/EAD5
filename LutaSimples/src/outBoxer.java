dcf7faca7a81dff3cf6f1eed97ac19dbdac04421
Rafael <0001157509@senaimgaluno.com.br>
2025-11-24 21:59:54 -0300

    Fim da atividade

--- LutaSimples/src/outBoxer.java
+++
@@ -1,8 +0,0 @@
-public class outBoxer extends lutador {
-    public outBoxer() {
-        super( "", 120, 60, 20, 50);
-    }
-    public void atacar(lutador inimigo){
-        System.out.println("");
-    }
-}
